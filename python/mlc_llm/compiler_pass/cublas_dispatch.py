85ed5c38a93549ee4397d35ff754d4ca6012bdac
Lesheng Jin <leshenj15@gmail.com>
2024-05-08 01:00:00 -0400

    merge

--- python/mlc_llm/compiler_pass/cublas_dispatch.py
+++ python/mlc_llm/compiler_pass/cublas_dispatch.py
@@ -1,6 +1,7 @@
 """A compiler pass that dispatches patterns to CUBLAS."""
 import tvm
 import tvm.relax.backend.contrib.cublas as _cublas
+import tvm.relax.backend.contrib.hipblas as _hipblas
 from tvm import IRModule, relax
 from tvm.relax.backend import get_patterns_with_prefix
 
@@ -16,6 +17,32 @@ class CublasDispatch:  # pylint: disable=too-few-public-methods,broad-exception-
             raise Exception("CUBLAS is not enabled.")
 
         patterns = get_patterns_with_prefix("cublas")
+
+        model_names = [
+            gv.name_hint for gv, func in mod.functions.items() if isinstance(func, relax.Function)
+        ]
+        mod = tvm.transform.Sequential(
+            [
+                relax.transform.FuseOpsByPattern(
+                    patterns, bind_constants=False, annotate_codegen=True
+                ),
+                relax.transform.RunCodegen({}, entry_functions=model_names),
+            ]
+        )(mod)
+        return mod
+
+
+@tvm.transform.module_pass(opt_level=0, name="HipblasDispatch")
+class HipblasDispatch:  # pylint: disable=too-few-public-methods,broad-exception-raised
+    """A compiler pass that dispatches patterns to CUBLAS."""
+
+    def transform_module(self, mod: IRModule, _ctx: tvm.transform.PassContext) -> IRModule:
+        """IRModule-level transformation"""
+        has_cublas = tvm.get_global_func("relax.ext.hipblas", True)
+        if not has_cublas:
+            raise Exception("HipBLAS is not enabled.")
+
+        patterns = get_patterns_with_prefix("hipblas")
 
         model_names = [
             gv.name_hint for gv, func in mod.functions.items() if isinstance(func, relax.Function)
